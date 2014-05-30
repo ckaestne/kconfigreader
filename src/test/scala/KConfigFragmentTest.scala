19a0cc5791b001eef6a081fdc9dab0c78b9522ed
Your Name <you@example.com>
2014-05-30 08:04:21 -0400

    add failing tests on string/int dependencies

--- src/test/scala/KConfigFragmentTest.scala
+++ src/test/scala/KConfigFragmentTest.scala
@@ -204,5 +204,11 @@ class KConfigFragmentTest extends DifferentialTesting {
     @Test def testDepExtra3() {
         checkTestModelBruteForce("src/test/resources/depextra3.config")
     }
+    @Ignore @Test def testNumDep() {
+        checkTestModelBruteForce("src/test/resources/numdep.config")
+    }
+    @Ignore @Test def testStringDep() {
+        checkTestModelBruteForce("src/test/resources/stringdep.config")
+    }
 
 }
